9cd08aef6eba1c5c7bf067558b425ed046a93f5b
Eddy Caldas <eddycaldas@live.com>
2017-09-04 12:09:12 -0600

    #3: Express math

--- server.js
+++ server.js
@@ -1,24 +1,19 @@
 
-var curlify = require('request-as-curl');
-var express = require('express');
-var app = express();
-var x;
-var y;
+// EXERCISE 03
+//Express Math: Volume with Path Variables
+const express = require('express')
+const app = express();
+const router = express.Router();
+const port = process.env.PORT || 3000
 
+router.get('/math/volume/:length/:width/:height', function(req, res) {
+  var side1 = req.params.length;
+  var side2 = req.params.width;
+  var side3 = req.params.height;
+  res.send('The volume of a ' + side1  + ' x ' + side2  + ' X ' + side3 + ' rectangle is ' + side1 * side2 * side3);
+});
 
-app.get('/math/pi', function(request, response) {
-  response.send('' + Math.PI)
-})
+app.listen(port);
+console.log(`doin some magic here : ${port}`);
 
-app.get('/math/pi', function (request, response) {
-  curlify(request);
-})
-
-
-  
-  
-
-
-app.listen(8080)
-
-
+app.use('/', router)
